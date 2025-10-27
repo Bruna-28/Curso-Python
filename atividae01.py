cea912ad4f6161640dd2a172db0b8cc27d4007ec
Bruna Lopes <brunadevs@yahoo.com>
2025-10-27 12:21:31 -0300

    Add rectangle area calculation to atividae01.py

--- atividae01.py
+++ atividae01.py
@@ -1,5 +1,5 @@
-base = float(input("Digite a base do triangulo: "))
-altura = float(input("Digite a altura do triangulo: "))
+base = float(input("Digite a base do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
 
 area = base * altura
 
