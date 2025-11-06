e657aa30e14c2218b5d8762cbf3df70bd1ad5aad
Bruna <brunadevs@yahoo.com>
2025-11-06 10:08:53 -0300

    Crie duas pastas para adicionar as provas do curso e outra para atividades do conteúdo.

---
+++ atividades/ativ1.py
@@ -0,0 +1,9 @@
+idade1 = int(input("Digite uma idade: "))
+idade2 = int(input("Digite uma idade: "))
+
+if idade1 > idade2:
+    print("É maior")
+elif idade1 < idade2:
+    print("É menor")
+else:
+    print("É igual")
