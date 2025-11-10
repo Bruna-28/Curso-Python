92ce4592dd08e745c2f2f6719f5858e3bec9f069
Bruna Lopes <brunadevs@yahoo.com>
2025-11-10 18:53:29 -0300

    Iniciei atividade 13, falta concluir a lógica.

---
+++ atividades/ativ13.py
@@ -0,0 +1,7 @@
+"""
+#Atividade 13:
+ - Sistema de Login Simples:
+    Desenvolva um programa que peça ao usuário um nome de usuário
+    e uma senha e use if para verificar se são iguais a "admin" e "1234", respectivamente."""
+    
+nome_usuario = input()
