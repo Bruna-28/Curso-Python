422a44b3ced959a88462329f6084a8bd6a619bb8
Bruna <brunadevs@yahoo.com>
2025-11-12 18:05:46 -0300

    Atividades 01 e 02 resolvidas.

---
+++ Mod01/atividades-While/ativ01.py
@@ -0,0 +1,13 @@
+"""
+#Atividade 01:
+ - Contagem de 1 a 10:
+    Crie um programa que use um laço while
+    para contar de 1 a 10 e exibir cada número.
+"""
+
+i = 0
+
+while i in range(10):
+    i+=1
+    print(i)
+    
