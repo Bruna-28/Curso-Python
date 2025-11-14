2207b318c37d0f355ed82a35c579ca4acc4f4a53
Bruna Lopes <brunadevs@yahoo.com>
2025-11-14 10:52:59 -0300

    Atualizando arquivo

--- Mod01/atividades-While/ativ03.py
+++ Mod01/atividades-While/ativ03.py
@@ -4,3 +4,14 @@
     Faça um programa que solicite um número ao usuário e use
     um laço while para exibir a tabuada desse número (de 1 a 10).
 """
+"""
+while i <= 10:
+    num2 += 1
+    print(num2)
+    
+- O Python entra no while e verifica se i <= 10.
+- Se for verdade (por exemplo, i = 0), ele executa o bloco.
+- Mas dentro do bloco você nunca altera o valor de i, então i 
+    continua sendo o mesmo.
+- Quando o loop volta para o topo, a condição ainda é verdadeira 
+    — e isso se repete para sempre."""
