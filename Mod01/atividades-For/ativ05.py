02ace103d080a72396705f7c48266cef473e0234
Bruna <brunadevs@yahoo.com>
2025-11-18 20:35:08 -0300

    Adicionando atividade 05, esta incompleto.

---
+++ Mod01/atividades-For/ativ05.py
@@ -0,0 +1,18 @@
+listItems = []
+
+while True:
+    option = input("""Escolha uma opção: 
+                   1 - Adicionar\n 
+                   2 - Verificar Itens\n 
+                   3 - Atualizar Itens\n 
+                   4 - Excluir Itens\n 
+                   5 - Sair""")
+    match option:
+        case "1"
+            item = input("Digite o item que você deseja adiciona: ")
+            listItems.append(item)
+        case "2"
+            listItems.
+        case "3"
+        case "4"
+        case "5"
