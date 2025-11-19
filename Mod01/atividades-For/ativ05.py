46443a7b8974cfedee0cc4a5cee16a7935274d51
Bruna <brunadevs@yahoo.com>
2025-11-19 07:42:28 -0300

    Atividade incompleta.

--- Mod01/atividades-For/ativ05.py
+++ Mod01/atividades-For/ativ05.py
@@ -1,18 +1,28 @@
-listItems = []
+listItens = []
 
 while True:
-    option = input("""Escolha uma opção: 
+    option = input(""" 
                    1 - Adicionar\n 
                    2 - Verificar Itens\n 
                    3 - Atualizar Itens\n 
                    4 - Excluir Itens\n 
-                   5 - Sair""")
+                   5 - Sair
+                   Escolha uma opção: """)
     match option:
-        case "1"
+        case "1":
             item = input("Digite o item que você deseja adiciona: ")
-            listItems.append(item)
-        case "2"
-            listItems.
-        case "3"
-        case "4"
-        case "5"
+            listItens.append(item)
+        case "2":
+        # 1 - Método len - Usamos para saber o tamanho da lista
+            print(f"Itens da lista: {(listItens.len())}")
+        case "3":
+            novoItem = input("Digite um novo item: ")
+            position = int(input(f"Em qual posição dessas opções: {listItens.len()}"))
+            listItens.insert(position, novoItem)
+        case "4":
+            removeItem = input(f"Qual item você deseja remover, desntro dessas opções: {listItens.len()}")
+            listItens.remove(removeItem)
+        case "5":
+            if option == "Sair":
+                print("Encerrado o programa!")
+                break
