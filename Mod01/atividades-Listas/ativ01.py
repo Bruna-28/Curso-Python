1098347d0da9362e95c0eb11eb1ed839ccc07a4d
Bruna Lopes <brunadevs@yahoo.com>
2025-12-02 14:26:40 -0300

    Finalizando atividade 01 de listas, ela foi bastante desafiadora.

--- Mod01/atividades-Listas/ativ01.py
+++ Mod01/atividades-Listas/ativ01.py
@@ -53,37 +53,62 @@ while True:
             
     # Atualiza os dados do usuário   
     elif option == "3":
-        procurar = input("Digite o nome do usuário que deseja atualizar: ").title()
+        procurar = input("Digite o nome do usuário: ").strip().title()
+        encontrado = False
 
         for usuario in usuarios:
-            if usuario[0] == procurar:
+            if len(usuario) >= 2 and usuario[0] == procurar:
 
-                print("O que deseja atualizar?")
-                print("Nome")
-                print("Email")
-                
-                escolha = input("Opção: ")
-                print(linha)
+                encontrado = True
+
+                print("O que você deseja atualizar?")
+                print("1 - Nome")
+                print("2 - Email")
+                escolha = input("Escolha: ")
+
 
                 # Atualizar nome
-                if escolha == "Nome":
+                if escolha == "1":
                     novoNome = input("Digite o novo nome: ").title()
                     usuario[0] = novoNome
                     print("Nome atualizado com sucesso!")
                     print(linha)
                     
                 # Atualizar email
-                elif escolha == "Email":
+                elif escolha == "2":
                     novoEmail = input("Digite o novo email: ")
                     usuario[1] = novoEmail
                     print("Email atualizado com sucesso!")
+                    print(usuario)
                     print(linha)
 
 
                 break  # sai do for assim que atualizar
         else:
             print("Usuário não encontrado!")
-    #elif option == "4":
+    elif option == "4":
+
+        usuariosRemover = input("Qual o novo do usuário que deseja remover? ").title()
+        encontrado = False
+        
+        #Percorrendo pelo lista     
+        for usuario in usuarios:
+            
+            #Verifica se o usuário foi encontrado
+            if usuario[0] == usuariosRemover:
+                print("Encontrado!")
+                print(f"Usuário: {usuario}")
+                removendo = usuario
+                break
+        else:
+                print("Não encotrado!")    
+                
+        #Removendo o usuário fora do loop
+        usuarios.remove(removendo)
+        print(f"usuário pagado!")
+    elif option == "5":
+        print("Programa encerrado!")
+        break
            
             
 
