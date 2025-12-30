a523c38e61b3c387e28bf1f5874f0a8c1d71ffa0
Bruna Kércia Lopes Siqueira <brunalopes@192.168.0.135>
2025-12-30 10:36:19 -0300

    Atividade 04 concluída.

---
+++ Mod01/atividades-Listas/ativ04.py
@@ -0,0 +1,11 @@
+import random
+
+palestrantes = (
+                ("Autora: Ana Silva, Tema da palestra: Segurança da Informação, Instituição: Instituto Federal de Tecnologia."),
+                ("Autor: Carlos Pereira, Tema da palestra: Boas Práticas no Uso da Tecnologia, Instituição: Instituto Federal de Tecnologia."),
+                ("Autora: Mariana Costa, Tema da palestra: Introdução à Ciência de Dados, Instituição: Universidade Tecnológica Nacional.")
+                )
+
+print(palestrantes[0])
+print(palestrantes[1])
+print(palestrantes[2])
