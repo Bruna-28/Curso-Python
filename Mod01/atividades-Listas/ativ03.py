cd32c3d43e2c08d5d15b8fa8356ceb0c1a1eb9ef
Bruna Kércia Lopes Siqueira <brunalopes@192.168.0.135>
2025-12-30 08:21:07 -0300

    Atividade 03 concluida.

---
+++ Mod01/atividades-Listas/ativ03.py
@@ -0,0 +1,10 @@
+#Lista com elementos aleatórios
+elementosAleatorios = ["Bruna", 29, "Fortaleza", True]
+# Exibindo apenas o 3 elemento da lista
+print(elementosAleatorios[2])
+
+#Acessando o ultimo elemento da lista
+print(elementosAleatorios[-1])
+
+# Acessando o penultimo elemento
+print(elementosAleatorios[-2])
