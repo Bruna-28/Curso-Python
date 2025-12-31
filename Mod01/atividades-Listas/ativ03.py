dfc01e8aec47a542c2dcba7e516bfc5997abe421
Bruna Kércia Lopes Siqueira <brunalopes@192.168.0.195>
2025-12-31 09:12:17 -0300

    Atualizando as atividades com adição de enunciados.

--- Mod01/atividades-Listas/ativ03.py
+++ Mod01/atividades-Listas/ativ03.py
@@ -1,3 +1,9 @@
+"""
+    Defina uma lista com 5 itens que tenha, pelo menos, 3
+    classes diferentes. Utilize o print() para exibir o
+    terceiro elemento dessa lista.
+"""
+
 #Lista com elementos aleatórios
 elementosAleatorios = ["Bruna", 29, "Fortaleza", True]
 # Exibindo apenas o 3 elemento da lista
